f9c757fe24548f7714efa153b48e648499eb640a
Ezra Lim <ezralim1992@gmail.com>
2025-01-20 23:38:32 +0800

    feat: Load test file and run in main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use neural_network::Neuron;
-
 fn main() {
-    let neuron = Neuron::new(1.0, vec![1.0, 2.0, 3.0]);
-    println!("Neuron: {:?}", neuron);
-    let inputs = vec![2.0, 3.0, 1.0];
-    let output = neuron.output(&inputs);
-    println!("Output: {}", output);
+    let network = neural_network::load_from_file("./test/fixtures/network.json");
+    let inputs = vec![0.0, 0.0];
+    let outputs = network.outputs(&inputs);
+    println!("{:?}", outputs);
 }
